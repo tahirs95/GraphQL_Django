980bad56575a5baaad084a050bf36e18d171d7f0
Tahir Shaukat <tahirs95@hotmail.com>
2019-07-03 01:55:56 -0700

    CRUD functionality added on Tracks.

--- tracks/admin.py
+++ tracks/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Track
+from .models import Track, Like
 
 # Register your models here.
 admin.site.register(Track)
+admin.site.register(Like)
+
--- tracks/schema.py
+++ tracks/schema.py
@@ -1,19 +1,29 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Track
+from graphql import GraphQLError
+from .models import Track, Like
+from users.schema import UserType
 
 class TrackType(DjangoObjectType):
     class Meta:
         model = Track
 
+class LikeType(DjangoObjectType):
+    class Meta:
+        model = Like
+
 class Query(graphene.ObjectType):
     tracks = graphene.List(TrackType)
+    likes = graphene.List(LikeType)
 
     def resolve_tracks(self, info):
         return Track.objects.all()
+    
+    def resolve_likes(self, info):
+        return Like.objects.all()
 
 class CreateTrack(graphene.Mutation):
-    post = graphene.Field(TrackType)
+    track = graphene.Field(TrackType)
 
     class Arguments:
         title = graphene.String()
@@ -21,14 +31,98 @@ class CreateTrack(graphene.Mutation):
         url = graphene.String()
 
     def mutate(self, info, **kwargs):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Log in to add a track.")
+
         title = kwargs.get('title')
         description = kwargs.get('description')
         url = kwargs.get('url')
 
-        track = Track(title=title, description=description, url=url)
+        track = Track(title=title, description=description, url=url, posted_by=user)
         track.save()
         return CreateTrack(track=track)
 
+class UpdateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Log in to add a track.")
+
+        track_id = kwargs.get('track_id')
+        title = kwargs.get('title')
+        description = kwargs.get('description')
+        url = kwargs.get('url')
+        track = Track.objects.get(id=track_id)
+
+        if track.posted_by != user:
+            raise Exception("Not permitted to update this track.")
+        
+        track.title = title
+        track.description = description
+        track.url = url
+
+        track.save()
+        return UpdateTrack(track=track)
+
+class DeleteTrack(graphene.Mutation):
+    track_id = graphene.Int()
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+
+    def mutate(self, info, **kwargs):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Log in to add a track.")
+
+        track_id = kwargs.get('track_id')
+        track = Track.objects.get(id=track_id)
+
+        if track.posted_by != user:
+            raise Exception("Not permitted to update this track.")
+    
+        track.delete()
+        return DeleteTrack(track_id=track_id)
+
+class CreateLike(graphene.Mutation):
+    user = graphene.Field(UserType)
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+
+    def mutate(self, info, **kwargs):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Log in to add a track.")
+
+        track_id = kwargs.get('track_id')
+        track = Track.objects.get(id=track_id)
+
+        if not track:
+            raise Exception("Can't find track with given track_id.")
+        
+        Like.objects.create(
+            user=user,
+            track=track
+
+        )
+    
+
+        return CreateLike(user=user, track=track)
+
 class Mutation(graphene.ObjectType):     
-    create_user = CreateUser.Field() 
-    create_post = CreatePost.Field()
+    create_track = CreateTrack.Field() 
+    update_track = UpdateTrack.Field()
+    delete_track = DeleteTrack.Field()
+    create_like = CreateLike.Field()
+     
